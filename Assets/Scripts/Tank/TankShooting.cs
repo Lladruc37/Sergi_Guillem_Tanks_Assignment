7fc8ec268151162bc96d57ace51757c9c070baad
Kiwi <59049734+WillyTrek19@users.noreply.github.com>
2021-11-22 15:02:26 +0100

    Started with assignment itself
    Shooting angle calculation & Ammo delay.
    
    Co-Authored-By: Lladruc <59049906+Lladruc37@users.noreply.github.com>

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -3,72 +3,80 @@ using UnityEngine.UI;
 
 public class TankShooting : MonoBehaviour
 {
-    public int m_PlayerNumber = 1;       
+    public int m_PlayerNumber = 1;
+    public GameObject tankTurret;
     public Rigidbody m_Shell;            
     public Transform m_FireTransform;    
-    public Slider m_AimSlider;           
     public AudioSource m_ShootingAudio;  
-    public AudioClip m_ChargingClip;     
     public AudioClip m_FireClip;         
-    public float m_MinLaunchForce = 15f; 
-    public float m_MaxLaunchForce = 30f; 
-    public float m_MaxChargeTime = 0.75f;
-
+    public float m_LaunchForce = 30f;
+    public float delayTime = 0.0f;
+    public int ammo = 5;
     
     private string m_FireButton;         
-    private float m_CurrentLaunchForce;  
-    private float m_ChargeSpeed;         
-    private bool m_Fired;                
+    private bool m_CanFire;
+    private float dt = 0.0f;
 
-
-    private void OnEnable()
-    {
-        m_CurrentLaunchForce = m_MinLaunchForce;
-        m_AimSlider.value = m_MinLaunchForce;
-    }
-
+    private float prevAngle = 0.0f;
+    private float angle = 0.0f;
+    private float g = -Physics.gravity.y;
 
     private void Start()
     {
         m_FireButton = "Fire" + m_PlayerNumber;
-
-        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+        m_CanFire = true;
+        angle = -1;
     }
 
     private void Update()
     {
-        m_AimSlider.value = m_MinLaunchForce;
+        float x = 5.0f;
+        float? angle1 = 0.0f;
+        float? angle2 = 0.0f;
+        float vel2 = Mathf.Pow(m_LaunchForce, 2);
+        angle1 = Mathf.Atan((vel2 + Mathf.Sqrt(vel2 * vel2 - g * g * x * x)) / (g * x)) * Mathf.Rad2Deg;
+        angle2 = Mathf.Atan((vel2 - Mathf.Sqrt(vel2 * vel2 - g * g * x * x)) / (g * x)) * Mathf.Rad2Deg;
 
-        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        if (angle1 != null && angle2 != null)
         {
-            m_CurrentLaunchForce = m_MaxLaunchForce;
+            prevAngle = angle;
+            if (angle1 <= angle2)
+            {
+                angle = (float)angle1;
+            }
+            else
+            {
+                angle = (float)angle2;
+            }
+            tankTurret.transform.Rotate(Vector3.left, angle - prevAngle);
+        }
+
+
+        if (angle != -1 && m_CanFire)
+        {
             Fire();
         }
-        else if (Input.GetButtonDown(m_FireButton))
+        else
         {
-            m_Fired = false;
-            m_CurrentLaunchForce = m_MinLaunchForce;
-
-            m_ShootingAudio.clip = m_ChargingClip;
-            m_ShootingAudio.Play();
-        }
-        else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
-        {
-            Fire();
+            dt += Time.deltaTime;
+            if (dt >= delayTime)
+            {
+                dt = 0.0f;
+                m_CanFire = true;
+            }
         }
     }
 
-
     private void Fire()
     {
-        m_Fired = true;
+        m_CanFire = false;
 
         Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation);
-        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+        shellInstance.velocity = m_LaunchForce * tankTurret.transform.forward;
 
         m_ShootingAudio.clip = m_FireClip;
         m_ShootingAudio.Play();
 
-        m_CurrentLaunchForce = m_MinLaunchForce;
+        ammo--;
     }
 }
